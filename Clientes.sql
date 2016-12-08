d21b3d1e451a118268a1ac6687c31b3543d71b98
Cristian Rodriguez <isc.rodriguezcristian@gmail.com>
2016-12-08 00:57:40 -0800

    Sql Server

--- Clientes.sql
+++ Clientes.sql
@@ -27,3 +27,22 @@ Create table Pagos
 	IVA int
 	foreign key (ClienteId) references Clientes(ClienteId)
 );
+
+Insert into EstadosClientes Values ('Activo')
+Insert into EstadosClientes Values ('Desactivado')
+
+SELECT * FROM EstadosClientes
+
+Insert into Clientes values ('Juan',18,1)
+Insert into Clientes values ('Maria',21,2)
+Insert into Clientes values ('Joselyn',33,1)
+Insert into Clientes values ('Miguel',2,2)
+
+SELECT * FROM Clientes
+
+Insert into Pagos values (1,GETDATE(),18,'Dolar',5)
+Insert into Pagos values (1,GETDATE(),100,'Dolar',5)
+Insert into Pagos values (4,GETDATE(),10,'Euro',10)
+
+
+SELECT * FROM Pagos
